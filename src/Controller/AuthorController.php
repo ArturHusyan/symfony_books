9f0dd80c082628ea51f89b9b9402b6cf237ee3cb
Artur Harutyunyan <artur.husyan@gmail.com>
2023-09-22 14:57:47 +0300

    route and controller to add books, getallbook

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Author;
+use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,13 @@ class AuthorController extends AbstractController
         return $this->render("addAuthorPage/addAuthorPage.html.twig", ["status" => null]);
     }
 
+    #[Route('/author/{id}', name: 'open_author_page')]
+    public function showAuthorPage(EntityManagerInterface $entityManager, $id)
+    {
+        $author = $entityManager->getRepository(Author::class)->find($id);
+        $authorBooks = $entityManager->getRepository(Book::class)->findBy(['author' => $id], ['title' => 'ASC']);
+            return $this->render("authorPage/authorPage.html.twig", ["author" => $author, "authorBooks" => $authorBooks, "status" => null]);
+    }
 
     #[Route('/add_author', name: 'add_author')]
     public function addAuthor(EntityManagerInterface $entityManager, Request $request)
@@ -37,8 +45,4 @@ class AuthorController extends AbstractController
         }
     }
 
-    // #[Route('/author', name: 'open_author_page')]
-    // public function index() {
-    //     return $this->render("authorPage/authorPage.html.twig", []);
-    // }
 }
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,10 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookController extends AbstractController
 {
-    
+    #[Route('/add_book/{author_id}', name: 'add_book')]
+    public function addBook(EntityManagerInterface $entityManager, Request $request, $author_id)
+    {
+        $title = $request->request->get('title');
+        $author = $entityManager->getRepository(Author::class)->find($author_id);
+
+        try {
+            $book = new Book();
+            $book->setTitle($title)
+                 ->setAuthor($author);
+            
+            $entityManager->persist($book);
+            $entityManager->flush();
+            
+            $authorBooks = $entityManager->getRepository(Book::class)->findBy(['author' => $author_id], ['title' => 'ASC']);
+            
+            return $this->render("authorPage/authorPage.html.twig", ["author" => $author, "authorBooks" => $authorBooks, "status" => "Книга успешно добавлена!"]);
+     
+        } catch(\Doctrine\ORM\ORMException $e) {
+            return $this->redirect($request->headers->get('referer'));
+        }
+    }
+   
 }
